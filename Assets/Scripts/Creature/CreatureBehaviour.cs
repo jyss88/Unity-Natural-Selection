f182cec49e46a80e602526d9b9df84393f8eecca
jyss88 <jordan.y.sim97@gmail.com>
2020-06-25 16:59:58 +1000

    Creature reproduction
    Creatures now reproduce (without mutation)

--- Assets/Scripts/Creature/CreatureBehaviour.cs
+++ Assets/Scripts/Creature/CreatureBehaviour.cs
@@ -9,11 +9,14 @@ public class CreatureBehaviour : MonoBehaviour, IEater
     [SerializeField] private AbilitySettings sizeSettings;
 #pragma warning restore 0649
 
+    private bool isSet = false;
+
     private ICreatureState state;
     private ICreatureMetabolism metabolism;
     private ICreatureMovement movement;
     private ICreatureSense sense;
     private ICreatureSize size;
+    private ICreatureFactory creatureFactory;
 
     public float Energy { get { return metabolism.Energy;} }
     public float Velocity { get { return movement.Velocity; } }
@@ -21,13 +24,14 @@ public class CreatureBehaviour : MonoBehaviour, IEater
     public float Size { get { return size.Size; } }
     public Collider2D Target { get { return sense.Target; } }
 
-    private void OnValidate() {
+    private void Awake() {
         state = new CreatureState();
         metabolism = new CreatureMetabolism(gameObject, settings.StartingEnergy);
 
         size = new CreatureSize(sizeSettings, settings.Size, metabolism, transform);
         sense = new CreatureSense(senseSettings, settings.SenseRadius, state, metabolism, transform);
         movement = new CreatureMovement(velocitySettings, settings.Velocity, state, metabolism, sense, transform);
+        creatureFactory = new CreatureFactory(gameObject, metabolism, transform, settings.Size, settings.Velocity, settings.SenseRadius);
     }
 
     // Update is called once per frame
@@ -36,9 +40,27 @@ public class CreatureBehaviour : MonoBehaviour, IEater
         movement.Tick();
         metabolism.Tick();
         sense.Tick();
+        size.Tick();
+        creatureFactory.Tick();
     }
 
     public void Eat(float energy) {
         metabolism.AddEnergy(energy);
+    }
+
+    public void SetAttributes(float startingEnergy, float size, float velocity, float senseRadius) {
+        if (!isSet) {
+            state = new CreatureState();
+            metabolism = new CreatureMetabolism(gameObject, startingEnergy);
+
+            this.size = new CreatureSize(sizeSettings, size, metabolism, transform);
+            sense = new CreatureSense(senseSettings, senseRadius, state, metabolism, transform);
+            movement = new CreatureMovement(velocitySettings, velocity, state, metabolism, sense, transform);
+            creatureFactory = new CreatureFactory(gameObject, metabolism, transform, size, velocity, senseRadius);
+
+            isSet = true;
+        } else {
+            Debug.Log("attributes have already been set.");
+        }
     }
 }
---
+++ Assets/Scripts/Creature/CreatureFactory.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class CreatureFactory : ICreatureFactory
+{
+    private ICreatureMetabolism metabolism;
+    private GameObject prefab;
+    private Transform transform;
+    private float size, velocity, sightRadius;
+    private float deltaMutate;
+
+    public CreatureFactory(GameObject prefab, ICreatureMetabolism metabolism, Transform transform, float size, float velocity, float sightRadius) {
+        this.prefab = prefab;
+
+        this.transform = transform;
+        this.metabolism = metabolism;
+        this.size = size;
+        this.velocity = velocity;
+        this.sightRadius = sightRadius;
+    }
+
+    public GameObject Reproduce() {
+        Vector3 offsetVector = new Vector3(transform.localScale.x, transform.localScale.y);
+        return Object.Instantiate(prefab, transform.position + offsetVector, Quaternion.identity);
+    }
+
+    public void Tick() {
+        if (metabolism.Energy > (metabolism.StartingEnergy + size * 10)) {
+            Reproduce();
+
+            metabolism.DecreaseEnergy(metabolism.StartingEnergy);
+        }
+    }
+}
