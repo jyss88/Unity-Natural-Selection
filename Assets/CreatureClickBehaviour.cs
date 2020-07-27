9940886a0bb0dc56a325961843b537f8fd54824b
jyss88 <jordan.y.sim97@gmail.com>
2020-07-27 16:38:00 +1000

    Creature menu
    Added menu for viewing creature attributes in game

---
+++ Assets/CreatureClickBehaviour.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+
+public class CreatureClickBehaviour : MonoBehaviour, IPointerClickHandler {
+    [SerializeField] private GameObject panel;
+    private CreatureInfoPanelBehaviour panelBehaviour;
+    private CreatureBehaviour creature;
+
+    public void OnPointerClick(PointerEventData eventData) {
+        Debug.Log("Clicked on creature");
+        panelBehaviour.Creature = creature;
+        panel.SetActive(!panel.activeSelf);
+    }
+
+    // Start is called before the first frame update
+    void Awake() {
+        creature = GetComponent<CreatureBehaviour>();
+        panelBehaviour = panel.GetComponent<CreatureInfoPanelBehaviour>();
+    }
+}
---
+++ Assets/CreatureInfoPanelBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class CreatureInfoPanelBehaviour : MonoBehaviour
+{
+    public CreatureBehaviour Creature { get; set; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Creature != null) {
+            transform.position = Camera.main.WorldToScreenPoint(Creature.transform.position);
+        } else if(gameObject.activeSelf) {
+            gameObject.SetActive(false);
+        }
+    }
+}
